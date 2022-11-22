c320425e7cee228b641d0125db55c3029499c250
Chay <chay@chay.name>
2022-11-22 15:37:51 +0000

    Typescript Header Edge Function

--- netlify/edge-functions/http-header.ts
+++ netlify/edge-functions/http-header.ts
@@ -1,5 +1,5 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-    request.headers.set("X-Powered-By", "Netlify+Cloudflare");
+    request.headers.set("X-Powered-By", "Netlify+Cloudflare+HTML");
 };
